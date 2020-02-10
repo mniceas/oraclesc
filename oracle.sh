94d864ab2cc51a2f964fe7c70bd5482300d0d72a
Appa <niceas49@hotmail.com>
2020-02-10 14:45:53 -0300

    Add files via upload

---
+++ oracle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Baixando os clients
+eval wget http://cdn1.netmake.com.br/download/Conexao/Oracle/Linux/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm && wget http://cdn1.netmake.com.br/download/Conexao/Oracle/Linux/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm
+
+#Instalando o Alien
+eval sudo apt-get install alien
+
+#Convertendo de RPM para DEB 
+eval sudo alien ~/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm && sudo alien ~/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm
+
+#Instalando os pacotes
+eval sudo dpkg -i oracle-instantclient12.1-basic_12.1.0.2.0-2_amd64.deb && sudo dpkg -i oracle-instantclient12.1-devel_12.1.0.2.0-2_amd64.deb
+
+#Recuperando as variáveis
+
+extdir=eval "php -i | grep extension_dir"
+phpinipath= "php -i | grep php.ini"
+
+#Baixando a extensão para PHP 7.3
