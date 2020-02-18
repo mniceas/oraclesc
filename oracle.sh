45135c80b2dc06ba981d0adc485d01736806a881
Matheus Nicéas <niceas49@hotmail.com>
2020-02-18 15:07:51 -0300

    Adicionando extensão ao php.ini

--- oracle.sh
+++ oracle.sh
@@ -13,13 +13,25 @@ eval sudo alien ~/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm && sudo
 eval sudo dpkg -i oracle-instantclient12.1-basic_12.1.0.2.0-2_amd64.deb && sudo dpkg -i oracle-instantclient12.1-devel_12.1.0.2.0-2_amd64.deb
 
 #Recuperando as variáveis
-extdir="$(eval php -i | grep extension_dir)"
-phpinipath="$eval php -i | grep php.ini"
+#extdir="$(eval php -i | grep extension_dir)"
+#phpinipath="$eval php -i | grep php.ini"
+#extpath = ${extdir}
 
-extpath = ${extdir}
+#Recuperando diretório de extensões
+declare extdir
+extdir=(`php -i | grep extension_dir`)
+#echo ${extdir[2]}
+
+#Recuperando diretório do php.ini
+declare phpdir
+phpdir=(`php -i | grep php.ini`)
+#echo ${phpdir[10]}
 
 #Baixando a extensão para PHP 7.3
 eval wget http://raptorgames.myscriptcase.com/oci8.so
 
 #Movendo extensão para o diretório correto
-eval sudo cp -avr oci8.so ${extpath}
+eval sudo cp -avr oci8.so ${extdir[2]}
+
+#Declarando variável no php.ini
+echo "extension=oci8.so" >> ${phpdir[10]}
