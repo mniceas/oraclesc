cd83df51337ef801581621c1fda00f6f04eda3b1
Matheus Nicéas <niceas49@hotmail.com>
2020-03-02 11:47:00 -0300

    Adição de retorno em ações do bash.

--- oracle.sh
+++ oracle.sh
@@ -11,7 +11,7 @@ eval wget $basic && wget $devel
 #Identificando versão do PHP
 declare phpv
 phpv=(`php -i | grep 'PHP Version' | cut -c 16-21`)
-#echo $phpv
+echo $phpv
 #Atualizando o sistema
 eval sudo apt-get -y update && apt-get -y upgrade
 #Instalando o Alien
@@ -24,7 +24,7 @@ eval sudo dpkg -i oracle-instantclient12.1-basic_12.1.0.2.0-2_amd64.deb && sudo 
 #Recuperando diretório de extensões
 declare extdir
 extdir=(`php -i | grep extension_dir`)
-#echo ${extdir[2]}
+echo ${extdir[2]}
 #Recuperando diretório do php.ini
 declare phpdir
 phpdir=(`php -i | grep 'php.ini' | cut -c 37-50`)
@@ -34,14 +34,20 @@ ini='apache2/php.ini'
 eval wget http://raptorgames.myscriptcase.com/oci8.so
 #Movendo extensão para o diretório correto
 eval sudo cp -avr oci8.so ${extdir[2]}
+eval ls -l ${extdir[2]}
 #Declarando variável no php.ini
+sudo echo "" >> $phpdir$ini
 sudo echo "extension=oci8.so" >> $phpdir$ini
 #Adicionando a diretiva LD_LIBRARY_PATH no envvars
 sudo echo "export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib/" >> /etc/apache2/envvars
 #Reiniciando o serviço do Apache
 eval sudo service apache2 restart;;
 
-n|N ) echo "Falow.";;
+n|N ) echo "Falow."
+echo "-Appa";;
 
-* ) echo "Bixo, digite direito.";;
+* ) echo "Bixo, digite direito..."
+echo "Bote y ou n."
+cpath=`pwd`
+eval sudo bash $cpath/oracle.sh;;
 esac
